8bc0a79a3609125d4959a92f8af71184013b39f6
tj <tomisinjoseph3@gmail.com>
2021-09-28 16:28:21 +0100

    Refactored tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,25 +1,25 @@
 describe('Thermostat', function() { 
 
-  let thermostat; 
+  let thermostat;
+  
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
  it('thermostat is an instance of a Thermostat', function() {
-  thermostat = new Thermostat();
   expect(thermostat).toBeInstanceOf(Thermostat);
  });
 
  it('is initialized with a temperature of 20 degrees', function() {
-  thermostat = new Thermostat();
   expect(thermostat.temperature).toEqual(20);
  });
 
  it('increases the temperature', function() {
-  thermostat = new Thermostat();
   thermostat.up(1);
   expect(thermostat.temperature).toEqual(21);
  });
  
  it('decreases the temperature', function() {
-  thermostat = new Thermostat();
   thermostat.down(1);
   expect(thermostat.temperature).toEqual(19);
  });
