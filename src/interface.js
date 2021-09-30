c20f25d1c47820e5bdfb7dc43a64508f3c53e8e5
Harry Parsons <hparsons93@gmail.com>
2021-09-30 14:55:39 +0100

    modifications to interface

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  document.querySelector(#temperature)
+  const updateTemperature = function() {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  }
+  
+  const thermostat = new Thermostat();
+  document.querySelector('#temperature').innerText = thermostat.temperature 
 
+  document.querySelector('#temperature-up').addEventListener('click', function() {
+  thermostat.up();
+  updateTemperature();
+  });
 
+  document.querySelector('#temperature-down').addEventListener('click', function() {
+  thermostat.down();
+  updateTemperature();
+  });
 
+  document.querySelector('#temperature-reset').addEventListener('click', function() {
+  thermostat.resetTemperature();
+  updateTemperature();
+  });
+  
+  document.querySelector('#powersaving-on').addEventListener('click', function() {
+  thermostat.switchPowerSavingModeOn();
+  document.querySelector('#power-saving-status').innerText = 'on'; 
+  
+  });
+
+  document.querySelector('#powersaving-off').addEventListener('click', function() {
+  thermostat.switchPowerSavingModeOff();
+  document.querySelector('#power-saving-status').innerText = 'off';
+  
+  });
+  
 
 });
